0aaa697eadf583c399e4ef11b51665a43a130eea
Akash Mukherjee <akash3.11.2000@gmail.com>
2024-04-18 07:56:34 +0530

    refactored the code of attempt one

--- four-balls-attempt-one/src/main/java/com/example/FourBallsGenerator.java
+++ four-balls-attempt-one/src/main/java/com/example/FourBallsGenerator.java
@@ -2,16 +2,16 @@ package com.example;
 
 import processing.core.PApplet;
 
-public class Main extends PApplet {
+public class FourBallsGenerator extends PApplet {
 
     public static final int WINDOW_WIDTH = 1080;
     public static final int WINDOW_HEIGHT = 720;
-    public static final int CIRCLE_DIAMETER = 10;
+    public static final int BALL_DIAMETER = 10;
     public static final int TOTAL_NO_OF_BALLS = 4;
     int absolutePositionX = 0;
 
     public static void main(String[] args) {
-        PApplet.main(Main.class, args);
+        PApplet.main(FourBallsGenerator.class, args);
     }
 
     @Override
@@ -25,12 +25,12 @@ public class Main extends PApplet {
         for (int ballNo = 1; ballNo <= TOTAL_NO_OF_BALLS; ballNo++) {
             int relativePositionX = absolutePositionX * ballNo;
             int relativePositionY = (WINDOW_HEIGHT * ballNo) / (TOTAL_NO_OF_BALLS + 1);
-            drawCircle(relativePositionX, relativePositionY, CIRCLE_DIAMETER);
+            drawBall(relativePositionX, relativePositionY, BALL_DIAMETER);
         }
         absolutePositionX++;
     }
 
-    private void drawCircle(int positionX, int positionY, int diameter) {
+    private void drawBall(int positionX, int positionY, int diameter) {
         ellipse(positionX, positionY, diameter, diameter);
     }
 }
